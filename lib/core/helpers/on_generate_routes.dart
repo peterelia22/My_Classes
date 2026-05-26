a290f15766395ab9dfdf08b1f228271469138238
peterelia22 <peterelia914@gmail.com>
2026-05-26 12:45:49 +0300

    Refactor main application structure and add routing for splash screen

---
+++ lib/core/helpers/on_generate_routes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_classes/features/splash/presentation/splash_view.dart';
+
+Route<dynamic> onGenerateRoutes(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
---
+++ lib/features/splash/presentation/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_classes/features/splash/presentation/views/widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  static const String routeName = '/splash';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SplashViewBody());
+  }
+}
